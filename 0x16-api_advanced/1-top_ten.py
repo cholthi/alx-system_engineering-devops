a8247858a50b892b7f2e423a4bf16e3d44bc4b97
cholthi <cholthipaul@gmail.com>
2024-01-16 16:01:32 +0200

    added advanced task

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contains top_ten function"""
+import requests
+import uuid
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    id = str(uuid.uuid4())
+    headers = {
+        "User-Agent": f"advanced_api/ python3 ({id})"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Contains recurse function"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    id = str(uuid.uuid4())
+    headers = {
+        "User-Agent": f"api_advanced:\
+python3 ({id})"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
